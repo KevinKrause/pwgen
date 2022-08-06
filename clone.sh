c6dd1e9148aac47b15807396cf8f2fcbdb4bfb80
Kevin Krause <kevin@the-cheat.local>
2022-08-06 16:33:35 +0200

    Make clone.sh somewhat smarter

--- clone.sh
+++ clone.sh
@@ -1,24 +1,27 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
+FINDARGS=(-maxdepth 1 -not -name . -not -name .. -not -name 'clone.sh' -not -name 'pwgen.py' -not -name 'words.txt')
+
 if [ -e .git ]; then
-  echo "There's a .git directory here - let's not do something stupid. Exiting."
+  echo "There's a .git directory here. Let's not do something stupid. Exiting."
   exit
 fi
 
-rm clone.sh pwgen.py words.txt
+if [ $( find . ${FINDARGS[@]} | wc -l ) -gt 0 ]; then
+  echo "There are other files around. Please start in an empty directory. Exiting."
+  exit
+fi
+
+rm -f clone.sh pwgen.py words.txt
 
 git init
 git remote add origin https://github.com/KevinKrause/pwgen.git
 git fetch
 git checkout -t origin/master
 
-find . \
-  -maxdepth 1 \
-  -not -name . \
-  -not -name .. \
-  -not -name 'clone.sh' \
-  -not -name 'pwgen.py' \
-  -not -name 'words.txt' \
-  -print0 \
-  | xargs -0 rm -r
+find . ${FINDARGS[@]} -print0 | xargs -0 rm -r
 
