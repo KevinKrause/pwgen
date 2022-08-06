373b33d7f5aa2d847a5181899db6a9b84eee9c95
Kevin Krause <kevin@the-cheat.local>
2022-08-06 13:06:36 +0200

    Formatting, pylint hints

--- pwgen.py
+++ pwgen.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+"""
+Simple dictionary based password generator
+"""
+
 from random   import seed
 from random   import randint
 from math     import floor
@@ -9,14 +13,20 @@ import os
 config = {
     "password_length": 18,
     "iterations": 5,
-    "password_minlength": 10, # Should not be lower than 7 (2 char word + 2 digits + 1 special char + 2 char word)
-    "password_maxlength": 30, # Should not be higher than 30 as there are few long words > 13 chars, resulting in low entropy
+     # Should not be lower than 7
+     # (2 char word + 2 digits + 1 special char + 2 char word)
+    "password_minlength": 10,
+     # Should not be higher than 30 as there are few long words > 13 chars,
+     # resulting in low entropy
+    "password_maxlength": 30,
     "specialchars": '!@#$%^&*()-=+;:<>,./?'
 }
 
 def get_word(wordlist):
-    i = randint(0, len(wordlist) - 1)
-    return wordlist[i].lower()
+    """
+    Return a random entry from a given list of words
+    """
+    return wordlist[randint(0, len(wordlist) - 1)].lower()
 
 parser = ArgumentParser()
 parser.add_argument(
@@ -43,41 +53,41 @@ pwcount  = args.pwcount
 
 # Load entire word list
 wordsdir = os.path.dirname(os.path.realpath(__file__))
-file = open(wordsdir + "/words.txt", "r")
-lines = {}
-while True:
-    line = file.readline()
-    if not line:
-        break
-    line = line.rstrip()
-    linelength = len(line)
-    if not linelength in lines:
-        lines[linelength] = []
-    lines[linelength].append(line)
-file.close()
+with open(wordsdir + "/words.txt", "r", encoding='utf-8') as file:
+    lines = {}
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        line = line.rstrip()
+        linelength = len(line)
+        if not linelength in lines:
+            lines[linelength] = []
+        lines[linelength].append(line)
+    file.close()
 
 words_total_length = pwlength - 3 # leave room for special char + 2 digits
 
 seed()
 
-for i in range(0, pwcount):
+for c in range(0, pwcount):
     # Recipe for pw construction: word + special char + 2 digits + word
     #                         or: word + 2 digits + special char + word
     # One of the words starts with a capital letter, the other doesn't
     # Both words are either the same length, or one is 1 char shorter
     # In this case, put the shorter word randomly to front or back
-    
+
     # Workaround for unintuitive Python round() function
-    if(words_total_length % 2 == 1):
-      first_word_length = floor(words_total_length / 2) + randint(0,1)
+    if words_total_length % 2 == 1:
+        first_word_length = floor(words_total_length / 2) + randint(0,1)
     else:
-      first_word_length = words_total_length / 2
+        first_word_length = words_total_length / 2
 
     parts = []
     parts.append(get_word(lines[first_word_length]))
     parts.append(get_word(lines[words_total_length - first_word_length]))
     specialchar = get_word(config["specialchars"])
-    digits = str(randint(0, 9)) + str(randint(0, 9))
+    DIGITS = str(randint(0, 9)) + str(randint(0, 9))
 
     # One word gets an uppercase first letter
     i = randint(0, 1)
@@ -85,8 +95,8 @@ for i in range(0, pwcount):
 
     # Special char comes before or after number
     if randint(0, 1) == 1:
-        password = parts[0] + specialchar + digits + parts[1]
+        password = parts[0] + specialchar + DIGITS + parts[1]
     else:
-        password = parts[0] + digits + specialchar + parts[1]
+        password = parts[0] + DIGITS + specialchar + parts[1]
 
     print(password)
